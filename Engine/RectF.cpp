24ec693266222a0102af24987eac57002493b882
LeHunter <LeHunter@outlook.de>
2017-10-24 12:54:37 +0200

    Added RectF and upgraded to new sdk version

---
+++ Engine/RectF.cpp
@@ -0,0 +1,18 @@
+#include "RectF.h"
+
+RectF::RectF( float _left, float _right, float _top, float _bottom ) :
+	left( _left ),
+	right( _right ),
+	top( _top ),
+	bottom( _bottom ) {}
+
+RectF::RectF( const Vec2& topLeft, const Vec2& bottomRight ) :
+	RectF( topLeft.x, bottomRight.x, topLeft.y, bottomRight.y ) {}
+
+RectF::RectF( const Vec2& topLeft, float width, float height ) :
+	RectF( topLeft, { topLeft.x + width, topLeft.y + height } ) {}
+
+bool RectF::isCollidingWithRect( const RectF & other ) const {
+	return right > other.left && left < other.right
+		&& top < bottom && bottom > other.top;
+}
---
+++ Engine/RectF.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Vec2.h"
+
+class RectF {
+public:
+	RectF() = default;
+	RectF( float _left, float _right, float _top, float _bottom );
+	RectF( const Vec2& topLeft, const Vec2& bottomRight );
+	RectF( const Vec2& topLeft, float width, float height );
+	bool isCollidingWithRect( const RectF& other ) const;
+public:
+	float left;
+	float right;
+	float top;
+	float bottom;
+};
